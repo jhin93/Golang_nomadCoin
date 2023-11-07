040faf90a6b8cef779e1fffcf8b17ddc9eb63c7b
jhin93 <scene1993@gmail.com>
2023-11-07 22:22:43 +0900

    string formatting

--- main.go
+++ main.go
@@ -2,15 +2,9 @@ package main
 
 import "fmt"
 
-func plus(a ...int) int {
-	total := 0
-	for _, item := range a {
-		total += item
-	}
-	return total
-}
-
 func main() {
-	result := plus(2, 3, 4, 5, 6, 7, 8, 9, 10)
-	fmt.Println(result)
+	name := "Nicolas!! Is my name"
+	for _, letter := range name {
+		fmt.Printf("%x\n", letter)
+	}
 }

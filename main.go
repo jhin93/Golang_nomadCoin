0dd7c6efa261181a10a289c32814b6b352018f1a
jhin93 <scene1993@gmail.com>
2023-11-07 22:11:06 +0900

    함수(인자) (리턴타입). 변수에 함수 리턴값 넣을때도 리턴 몇개하는 지 체크

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func plus(a, b int, name string) (int, string) {
+	return a + b, name
+}
+
 func main() {
-	fmt.Println("It works!")
+	result, name := plus(2, 2, "nico")
+	fmt.Println(result, name)
 }

67bb978b1dd3c7f12f72e4ac2ebf586d9057229a
jhin93 <scene1993@gmail.com>
2023-11-12 23:31:55 +0900

    블록체인 구현 준비

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/jhin93/Golang_nomadCoin/person"
-)
-
 func main() {
-	nico := person.Person{}
-	nico.SetDetails("nico", 12)
-	fmt.Println("Main's nico : ", nico)
+
 }

784eea49927e3843ebaf8cc61e98214ce314380a
jhin93 <scene1993@gmail.com>
2023-11-12 21:18:28 +0900

    struct, 메소드 활용 예제

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) sayHello() { // 'person' struct의 메소드
+	fmt.Printf("Hello! my name is %s and i'm %d", p.name, p.age) // .Println으로 출력하면 '...name is %s and i'm %d' 라고 출력됨.
+	// %s, %d 는 각각 string, decimal(10진수)를 의미함. 순서대로 %s에는 p.name, %d에는 p.age가 할당됨
+}
+
 func main() {
-	a := 2
-	b := &a // b의 타입은 int pointer. var b *int
-	a = 50
-	a = 12
-	fmt.Println(*b)
+	nico := person{name: "nico", age: 12}
+	nico.sayHello()
 }

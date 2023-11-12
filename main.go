af8597c0e1029a19516ff853347043653cebb8ca
jhin93 <scene1993@gmail.com>
2023-11-12 23:02:24 +0900

    주석 삭제

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	nico := person.Person{}             // nico 변수에 'Person' type 할당
-	nico.SetDetails("nico", 12)         // 결과 : SeeDetails nico : {nico 12}
-	fmt.Println("Main's nico : ", nico) // 결과 : Main's nico' { 0}
+	nico := person.Person{}
+	nico.SetDetails("nico", 12)
+	fmt.Println("Main's nico : ", nico)
 }

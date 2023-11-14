8fd46b802e18f34ccb9092b5ed700da606b7b1be
jhin93 <scene1993@gmail.com>
2023-11-14 23:55:06 +0900

    data byte를 hash로 전환

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 type block struct {
 	data     string
@@ -10,5 +13,8 @@ type block struct {
 
 func main() {
 	genesisBlock := block{"Genesis Block", "", ""}
-	genesisBlock.hash = sha256.Sum256([]byte(genesisBlock.data))
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash)) // 1. byte 값 가져오기(data + prevHash) + sha256으로 난수화
+	hexHash := fmt.Sprintf("%x", hash)                                       // 2. 16진수 string으로 변환(.Sprintf("%x"))
+	genesisBlock.hash = hexHash                                              // 3. hash에 담음
+	fmt.Println(genesisBlock)
 }

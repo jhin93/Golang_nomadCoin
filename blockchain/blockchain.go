62be94d2d39666d078c1d608d6d304569e57a25f
jhin93 <scene1993@gmail.com>
2023-11-25 17:47:54 +0900

    4.4 강의 완료. 블록체인에 블록 추가하는 함수 필요

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,10 @@
 package blockchain
 
-import "sync"
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
 
 type block struct {
 	data     string
@@ -9,17 +13,37 @@ type block struct {
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block // blocks는 pointer의 slice
 }
 
 var b *blockchain
 var once sync.Once // sync 패키지의 type 'Once'
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.data + b.prevHash))
+	b.hash = fmt.Sprintf("%x", hash) // 16진수
+}
+
+func getLastHash() string { // 마지막 블록 해쉬 반환
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks-1].hash
+}
+
+func createBlock(data string) *block { // block 타입의 pointer 반환
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
 
 func GetBlockchain() *blockchain { // 변수 b와 동일한 타입인 blockchain의 pointer 반환
 	// 1. b 변수 초기화 여부 확인
 	if b == nil {
 		once.Do(func() { // 내부의 로직을 오직 한번만 실행(.Do)
 			b = &blockchain{} // 2. blockchain 인스턴스 생성
+			b.blocks = append(b.blocks, createBlock("Genesis Block"))
 		})
 	}
 	return b // 3. b 반환

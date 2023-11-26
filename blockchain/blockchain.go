401a71b271cefeee3a609e0434114a21441ef07d
jhin93 <scene1993@gmail.com>
2023-11-27 00:24:59 +0900

    4.5 완료

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -7,9 +7,9 @@ import (
 )
 
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
@@ -20,8 +20,8 @@ var b *blockchain
 var once sync.Once // sync 패키지의 type 'Once'
 
 func (b *block) calculateHash() {
-	hash := sha256.Sum256([]byte(b.data + b.prevHash))
-	b.hash = fmt.Sprintf("%x", hash) // 16진수
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash) // 16진수
 }
 
 func getLastHash() string { // 마지막 블록 해쉬 반환
@@ -29,7 +29,7 @@ func getLastHash() string { // 마지막 블록 해쉬 반환
 	if totalBlocks == 0 {
 		return ""
 	}
-	return GetBlockchain().blocks[totalBlocks-1].hash
+	return GetBlockchain().blocks[totalBlocks-1].Hash
 }
 
 func createBlock(data string) *block { // block 타입의 pointer 반환
@@ -38,16 +38,21 @@ func createBlock(data string) *block { // block 타입의 pointer 반환
 	return &newBlock // 반환값이 메모리 연산자인 이유는 createBlock 함수가 사용될 blockchain구조체의 내부 요소 blocks가 포인터 변수 슬라이스이기 때문
 }
 
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
 func GetBlockchain() *blockchain { // 변수 b와 동일한 타입인 blockchain의 pointer 반환
 	// 1. b 변수 초기화 여부 확인
 	if b == nil {
 		once.Do(func() { // 내부의 로직을 오직 한번만 실행(.Do)
 			b = &blockchain{} // 2. blockchain 인스턴스 생성
-			b.blocks = append(b.blocks, createBlock("Genesis Block"))
+			b.AddBlock("Genesis")
 		})
 	}
 	return b // 3. b 반환
 }
 
-// 누군가 이 blockchain을 처음으로 요청하면 nil을 반환하는 일 없이 blockchain을 먼저 초기화 한 뒤에 반환함
-// 이미 실행된 blockchain을 누군가 초기화하려고 해도 이미 실행됐던 GetBlockchain 함수로 인해 이미 nil이 아닌 b가 반환된다
+func (b *blockchain) AllBlocks() []*block {
+	return GetBlockchain().blocks
+}

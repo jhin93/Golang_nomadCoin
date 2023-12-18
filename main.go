f1a095d38eee91de678a6738b3fcd85513f26218
jhin93 <scene1993@gmail.com>
2023-12-18 22:12:07 +0900

    5.4 블록체인 추가 기능 완료

--- main.go
+++ main.go
@@ -28,10 +28,23 @@ func home(rw http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(rw, "home", data)                      // 'templates'변수에 'ExecuteTemplate'메소드를 적용 'home'이라는 페이지를 data 변수를 적용해서 실행한다.
 }
 
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil) // GET 요청은 단순히 "add" 템플릿 실행
+	case "POST":
+		r.ParseForm()                                           // HTTP 요청의 폼 데이터를 파싱하는 것입니다. ParseForm을 호출하면, 요청에 포함된 URL 쿼리 문자열과 요청 본문(body)의 폼 데이터가 파싱되어 Request 객체의 Form 필드에 저장됩니다
+		data := r.Form.Get("blockData")                         // Request 객체의 Form 필드 중 "blockData" 라는 input에서 온 데이터를 변수 'data'에 담음.
+		blockchain.GetBlockchain().AddBlock(data)               // 입력된 'blockData' 인풋의 데이터를 블록으로 추가.
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect) // 위 로직 실행 후 홈페이지로 리다이렉트하는 로직
+	}
+}
+
 func main() {
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))     // pages에 있는  모든 파일을 load한 결과물을 route안에서 호출.
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml")) // '/partial'에 있는 파일도 불러오기 위해 모든 파일이 담긴 'templates' 변수를 업데이트.
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	// .Fatal은 os.Exit(1) 다음에 따라나오는 error를 Print()
 	// os.Exit(1)은 프로그램이 error code 1으로 종료되는 것

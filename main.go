ea75ebad6e6f7acc67e9adac2b5d1d4f8fc16149
jhin93 <scene1993@gmail.com>
2024-01-25 19:35:28 +0900

    줄바꿈 변경

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	if len(os.Args) < 2 { // os.Args는 'go run main.go' 뒤에 붙은 argument를 의미한다. Ex) go run main.go rest, go run main.go explorer
 		fmt.Printf("Welcome to 노마드 코인\n\n")
 		fmt.Printf("Please use the following commands:\n\n")
-		fmt.Printf("explorer:	Start the HTML Explorer\n\n")
+		fmt.Printf("explorer:	Start the HTML Explorer\n")
 		fmt.Printf("rest:		Start the REST API (recommended)\n\n")
 	}
 }

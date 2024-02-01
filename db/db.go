bd82e3129791f88474ad6acaee661160ca9587ec
jhin93 <scene1993@gmail.com>
2024-02-01 20:07:09 +0900

    Opening a database with bolt

--- db/db.go
+++ db/db.go
@@ -1,1 +1,19 @@
 package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/jhin93/Golang_nomadCoin/utils"
+)
+
+// blockchain.go 처럼 singleton 패턴을 사용
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open("blockchain.db", 0600, nil) // https://github.com/boltdb/bolt?tab=readme-ov-file#opening-a-database
+		utils.HandleErr(err)
+		db = dbPointer
+	}
+	return db
+}
